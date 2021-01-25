cd1c78573872ce9a05ed457652978cd5fdf81e03
StrIvanSer <svet42@inbox.ru>
2021-01-25 10:24:57 +0500

    module 4 урок 3

--- src/main/java/com/example/MyBookShopApp/controllers/HTTPController.java
+++ src/main/java/com/example/MyBookShopApp/controllers/HTTPController.java
@@ -6,25 +6,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HTTPController {
 
-    @GetMapping("/")
-    public String getMainPage() {
-        return "redirect:/bookshop/main";
-    }
-
-    @GetMapping("index.html")
-    public String getIndex() {
-        return "redirect:/bookshop/main";
-    }
-
-    @GetMapping("/genres/index.html")
-    public String getGenres() {
-        return "redirect:/bookshop/genres";
-    }
-
-
-    @GetMapping("/authors/index.html")
-    public String getAuthors() {
-        return "redirect:/authors";
-    }
+//    @GetMapping("/")
+//    public String getMainPage() {
+//        return "redirect:/bookshop/main";
+//    }
+//
+//    @GetMapping("index.html")
+//    public String getIndex() {
+//        return "redirect:/bookshop/main";
+//    }
+//
+//    @GetMapping("/genres/index.html")
+//    public String getGenres() {
+//        return "redirect:/bookshop/genres";
+//    }
+//
+//
+//    @GetMapping("/authors/index.html")
+//    public String getAuthors() {
+//        return "redirect:/authors";
+//    }
 
 }
