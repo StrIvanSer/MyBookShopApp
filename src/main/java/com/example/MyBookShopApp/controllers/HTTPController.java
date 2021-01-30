1ec25da963f34bd4d936b86bdffccec00b9b7dca
StrIvanSer <svet42@inbox.ru>
2021-01-30 22:29:42 +0500

    module 4 урок 4

--- src/main/java/com/example/MyBookShopApp/controllers/HTTPController.java
+++ src/main/java/com/example/MyBookShopApp/controllers/HTTPController.java
@@ -3,7 +3,7 @@ package com.example.MyBookShopApp.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+//@Controller
 public class HTTPController {
 
 //    @GetMapping("/")
