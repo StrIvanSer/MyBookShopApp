1ba81c6b453f6f5377f3d53f5734b5e2728f9227
StrIvanSer <svet42@inbox.ru>
2021-04-19 11:29:58 +0500

    module 8 Добавил шедулер для удаления старых токенов один раз в неделю, в понедельник в 00:00

--- src/main/java/com/example/MyBookShopApp/secutiry/jwt/JWTBlackListRepository.java
+++ src/main/java/com/example/MyBookShopApp/secutiry/jwt/JWTBlackListRepository.java
@@ -1,10 +1,21 @@
 package com.example.MyBookShopApp.secutiry.jwt;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface JWTBlackListRepository extends JpaRepository<JWTBlackList,Long> {
+public interface JWTBlackListRepository extends JpaRepository<JWTBlackList, Long> {
+
+    @Query(value = "DELETE FROM public.token WHERE created_at < date_trunc('week', now())- INTERVAL '7days' ",
+            nativeQuery = true)
+    void deleteOldTokens();
+
+    @Query(value = "SELECT * FROM public.token WHERE created_at < date_trunc('week', now())- INTERVAL '7days' ",
+            nativeQuery = true)
+    List<JWTBlackList> getOldTokens();
 
     JWTBlackList findJwtBlacklistByToken(String token);
 }
